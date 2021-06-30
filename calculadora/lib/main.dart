cc2bfc1975a48ee3f10617c31c7c64d30432b35f
Juan Camilo Blanco Martinez <camilo3198@gmail.com>
2021-06-29 20:18:02 -0500

    initial project

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:calculadora/screens/calculator_screen.dart';
 import 'package:flutter/material.dart';
-import 'app.dart';
 
-void main() {
-  runApp(MyApp());
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: CalculatorScreen(),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black
+      ),
+    );
+  }
 }
